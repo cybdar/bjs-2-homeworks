050e295a45bc246d07ba7c7e44bec7748661d39e
cybdar <kari4ka21@gmail.com>
2026-02-28 00:39:27 +0200

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -20,7 +20,7 @@ function solveEquation(a, b, c) {
 	}
 }
 
-// ЗАДАЧА 22
+// ЗАДАЧА 2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	percent = Number(percent);
 	contribution = Number(contribution);
