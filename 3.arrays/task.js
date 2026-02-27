6385df3c49a9125174f353afa1e223092def013f
cybdar <kari4ka21@gmail.com>
2026-02-28 01:51:17 +0200

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
+'use strict';
+
+// Задача 1
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) return false;
+	return arr1.every((item, index) => item === arr2[index]);
 }
 
+// Задача 2
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filtered = users.filter(user => user.gender === gender);
+	if (filtered.length === 0) return 0;
+	return filtered.reduce((sum, user) => sum + user.age, 0) / filtered.length;
 }
